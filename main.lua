fd88ca0c1c4f873a96de6fe78e72ae4f8cebcb90
Fernando Figueiredo Drummond Bittencourt <fernandodbittencourt@hotmail.com>
2018-06-15 20:46:45 -0300

    14bis included.

--- main.lua
+++ main.lua
@@ -1,8 +1,15 @@
+plane_14bis = {
+	src = "images/14bis.png",
+	height = 64,
+	width = 64
+}
+
 function love.load()
 	love.window.setMode(320, 480, {resizable =false})
 	love.window.setTitle("14 bis vs meteor")
 
 	background = love.graphics.newImage("images/background.png")
+	plane_14bis.image = love.graphics.newImage(plane_14bis.src)
 
     x, y, w, h = 20, 20, 60, 20
 end
@@ -23,6 +30,6 @@ function love.update(dt)
 end
 
 function love.draw()
-	love.graphics.draw(background, 0, 0)
-    love.graphics.rectangle("fill", x, y, w, h)
+	love.graphics.draw(background, 0, 0)	
+	love.graphics.draw(plane_14bis.image, x, y)
 end
