d570d86d24470af60444c071b2fbf2e847cee9a5
Fernando Figueiredo Drummond Bittencourt <fernandodbittencourt@hotmail.com>
2018-06-15 20:52:58 -0300

    Organizing the controls.

--- main.lua
+++ main.lua
@@ -1,8 +1,25 @@
 plane_14bis = {
 	src = "images/14bis.png",
 	height = 64,
-	width = 64
+	width = 64,
+	x = 0, 
+	y = 0
 }
+
+function move14bis()
+	if love.keyboard.isDown('w') then
+		plane_14bis.y = plane_14bis.y - 1
+	end
+	if love.keyboard.isDown('s') then
+		plane_14bis.y = plane_14bis.y + 1
+	end
+	if love.keyboard.isDown('a') then
+		plane_14bis.x = plane_14bis.x - 1
+	end
+	if love.keyboard.isDown('d') then
+		plane_14bis.x = plane_14bis.x + 1
+	end
+end
 
 function love.load()
 	love.window.setMode(320, 480, {resizable =false})
@@ -11,25 +28,15 @@ function love.load()
 	background = love.graphics.newImage("images/background.png")
 	plane_14bis.image = love.graphics.newImage(plane_14bis.src)
 
-    x, y, w, h = 20, 20, 60, 20
 end
 
 function love.update(dt)
-	if love.keyboard.isDown('w') then
-		y = y - 1
-	end
-	if love.keyboard.isDown('s') then
-		y = y + 1
-	end
-	if love.keyboard.isDown('a') then
-		x = x - 1
-	end
-	if love.keyboard.isDown('d') then
-		x = x + 1
+	if love.keyboard.isDown('w','a','s','d') then
+		move14bis()
 	end
 end
 
 function love.draw()
 	love.graphics.draw(background, 0, 0)	
-	love.graphics.draw(plane_14bis.image, x, y)
+	love.graphics.draw(plane_14bis.image, plane_14bis.x, plane_14bis.y)
 end
