8cb2027da7335d39388cb14f12f881276bbc7d6c
Fernando Figueiredo Drummond Bittencourt <fernandodbittencourt@hotmail.com>
2018-06-15 21:35:35 -0300

    Meteor included.

--- main.lua
+++ main.lua
@@ -1,13 +1,38 @@
 WIDTH_SCREEN = 320
 HEIGHT_SCREEN = 480
+MAX_METEOR = 12
 
 plane_14bis = {
 	src = "images/14bis.png",
 	height = 64,
 	width = 64,
-	x = WIDTH_SCREEN - 64 / 2, 
-	y = HEIGHT_SCREEN/2 - 64 / 2
+	x = WIDTH_SCREEN/2 - 64 / 2, 
+	y = HEIGHT_SCREEN - 64 / 2
 }
+
+meteors = {}
+
+function createMeteor()
+	meteor = {
+		x = math.random(WIDTH_SCREEN),
+		y = 0
+	}
+	table.insert(meteors,meteor)
+end
+
+function moveMeteor()
+	for k, meteor in pairs(meteors) do
+		meteor.y = meteor.y +1;
+	end
+end
+
+function removeMeteors()
+	for i = #meteors, 1, -1 do
+		if meteors[i].y > HEIGHT_SCREEN then
+			table.remove(meteors, i)
+		end
+	end
+end
 
 function move14bis()
 	if love.keyboard.isDown('w') then
@@ -30,6 +55,7 @@ function love.load()
 
 	background = love.graphics.newImage("images/background.png")
 	plane_14bis.image = love.graphics.newImage(plane_14bis.src)
+	meteor_img = love.graphics.newImage("images/meteor.png")
 
 end
 
@@ -37,9 +63,18 @@ function love.update(dt)
 	if love.keyboard.isDown('w','a','s','d') then
 		move14bis()
 	end
+
+	removeMeteors()
+	if #meteors < MAX_METEOR then
+		createMeteor()
+	end
+	moveMeteor()
 end
 
 function love.draw()
 	love.graphics.draw(background, 0, 0)	
 	love.graphics.draw(plane_14bis.image, plane_14bis.x, plane_14bis.y)
+	for k,meteor in pairs(meteors) do
+		love.graphics.draw(meteor_img, meteor.x, meteor.y)
+	end
 end
